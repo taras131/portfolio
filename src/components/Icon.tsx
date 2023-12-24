b7822b01fd241910fe60e16812f6e7a13f8b7cfb
taras131 <mossnabitkana@gmail.com>
2023-12-24 22:39:34 +0300

    return_icon_props_isactive

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -1,23 +1,35 @@
 import React, {FC} from 'react';
 import sprite from "../assets/icons/icons-sprite.svg";
+import styled, {css} from "styled-components";
 import {spriteIds} from "../utils/consts";
 
 type TProps = {
     iconId: spriteIds,
     width?: number,
     height?: number,
+    isActive?: boolean,
 }
 
 export const Icon: FC<TProps> = ({
                                      iconId,
                                      width = 74,
                                      height = 74,
+                                     isActive = false,
                                  }) => {
     return (
-        <svg width={`${width}px`} height={`${height}px`} viewBox={`0 0 ${width} ${height}`}
+        <SVG width={`${width}px`} height={`${height}px`} viewBox={`0 0 ${width} ${height}`} isActive={isActive}
              xmlns="http://www.w3.org/2000/svg">
             <use xlinkHref={`${sprite}#${iconId}`}/>
-        </svg>
+        </SVG>
     );
 };
 
+type TStyledProps = {
+    isActive: boolean,
+}
+
+const SVG = styled.svg<TStyledProps>`
+  ${props => props.isActive && css<TStyledProps>`
+    fill: ${props => props.theme.colors.accent};
+  `}
+`
